cf79ffef36bfd30badc55388db9940a83757cafe
shalomfriss <shalomfriss@gmail.com>
2020-10-18 19:26:55 -0500

    cleanup

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 //Kudos: https://github.com/DatanewsOrg/google-news-js
 //https://github.com/sindresorhus/terminal-link
-const Parser        = require('rss-parser');
 const readline      = require("readline");
 const shell         = require("shelljs");
 const program       = require("commander");
@@ -10,9 +9,8 @@ const chalk         = require("chalk");
 const boxen         = require("boxen");
 
 
-const runPrompt = require('./lib/questions');
+const { runPrompt, TOPICS } = require('./lib/questions');
 
-const parser        = new Parser();
 
 const HEADLINES_RSS = 'https://news.google.com/news/rss';
 const TOPICS_RSS    = 'https://news.google.com/news/rss/headlines/section/topic/';
@@ -21,21 +19,18 @@ const SEARCH_RSS    = 'https://news.google.com/rss/search?q=';
 
 program.version('1.0.0');
 program.option('-n, --headlines <num>', 'Show top n headlines');
-program.option('-t, --topic <topic>', 'Show a topic');
+program.option('-t, --topic <topic> <result_number>', 'Show a topic');
 program.option('-l, --list', 'Show a list of topics');
 program.parse(process.argv);
 
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-
-
 if(program.headlines) {
-  console.log('headlines')
   showHeadlines(program.headlines);
+  return  
+}
+
+if(program.topic) {
+  console.log(program.topic)
+  //showHeadlines(program.topic);
   return  
 }
 
@@ -55,7 +50,7 @@ const handleEscKeypress = () => {
     }
   });
 };
-handleEscKeypress();
 
+handleEscKeypress();
 runPrompt();
 
